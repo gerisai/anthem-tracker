681a826122d8e8f7924a0b644f9edfebd727cecd
gerardoisai <isai.diaz.perez@alumnos.upm.es>
2023-07-19 06:56:44 -0600

    Add Chakra UI setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Providers } from "./providers"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
