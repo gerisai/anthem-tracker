56583bd0122301502fe25b30bc7b28ba36fd8ae6
gerardoisai <isai.diaz.perez@alumnos.upm.es>
2023-07-20 06:24:16 -0600

    Update login page for dark mode

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,11 +5,11 @@ import { Flex, Input, Heading, Button } from "@chakra-ui/react"
 export default function Login(){
     return (
     <Flex height="100vh" alignItems="center" justifyContent="center">
-        <Flex direction="column" background="gray.100" p={12} rounded={6}>
-            <Heading mb={6}>Iniciar Sesión</Heading>
-            <Input placeholder="example@email.com" variant="outline" mb={3} type="email" />
-            <Input placeholder="********" variant="outline" mb={3} type="password"  />
-            <Button colorScheme="teal">Entrar</Button>
+        <Flex direction="column" background="gray.700" p={12} rounded={6}>
+            <Heading color="white" mb={6}>Iniciar Sesión</Heading>
+            <Input color="white" placeholder="example@email.com" variant="outline" mb={3} type="email" />
+            <Input color="white" placeholder="********" variant="outline" mb={3} type="password"  />
+            <Button colorScheme="blue">Entrar</Button>
         </Flex>
     </Flex>
     )
