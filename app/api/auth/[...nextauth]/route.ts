82401a727707fb6be15f0bf44c6394fe6c8d0179
gerardoisai <isai.diaz.perez@alumnos.upm.es>
2023-07-27 22:00:24 -0600

    Add Google signIn method

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
@@ -14,7 +15,11 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GITHUB_ID!,
             clientSecret: process.env.GITHUB_SECRET!,
         }),
-    ]
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
 };
 
 const handler = NextAuth(authOptions);
--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,19 +1,35 @@
 'use client'
 
 import { Flex, Heading, Button } from "@chakra-ui/react"
+import { BiLogoGithub } from "react-icons/bi";
+import { FcGoogle } from "react-icons/fc";
 import { signIn } from 'next-auth/react'
 
 export default function SignIn({ providers }: { providers: any}){
+    const providerTheming: any = {
+        github: {
+            scheme: 'blackAlpha',
+            icon: <BiLogoGithub/>,
+        },
+        google: {
+            scheme: 'gray',
+            icon: <FcGoogle/>,
+        }
+    }
 
-    console.log(providers)
     return (
     <Flex height="100vh" alignItems="center" justifyContent="center">
         <Flex direction="column" background="gray.700" p={12} rounded={6}>
             <Heading color="white" mb={6}>Iniciar Sesión</Heading>
             {
                 Object.values(providers).map((provider: any) => (
-                    <Button colorScheme="teal" onClick={async () => await signIn(provider.id,{ redirect: true,
-                        callbackUrl: '/'})}>{provider.name}</Button>
+                    <Button 
+                        key={provider.id} 
+                        leftIcon={providerTheming[provider.id].icon}
+                        colorScheme={providerTheming[provider.id].scheme}
+                        onClick={async () => await signIn(provider.id,{ redirect: true, callbackUrl: '/'})} 
+                        my={2}>{provider.name}
+                    </Button>
                 ))
             }
         </Flex>
