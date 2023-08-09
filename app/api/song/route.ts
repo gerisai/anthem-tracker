75397234f258a99a36b661ad06a644e7a5094dc1
gerardoisai <isai.diaz.perez@alumnos.upm.es>
2023-08-09 07:50:45 -0600

    Delete unused session for song creation

--- app/api/song/route.ts
+++ app/api/song/route.ts
@@ -1,12 +1,8 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { prisma } from '@/lib/prisma';
-import { authOptions } from "../auth/[...nextauth]/route"
 
 export async function POST(req: Request) {
-  const session = await getServerSession(authOptions);
-  const currentUserEmail = session?.user?.email!;
-
   const data = await req.json();
 
   const song = await prisma.song.create({ data });
