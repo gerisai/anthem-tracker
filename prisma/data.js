12247ead6c68616ef02d6d38e348a86c29942dd1
gerardoisai <isai.diaz.perez@alumnos.upm.es>
2023-08-09 07:51:43 -0600

    Populate more songs

--- prisma/data.js
+++ prisma/data.js
@@ -2,7 +2,7 @@ const anthems =
 [
     {
         number: '1',
-        name: 'Cada momento la vida me da',
+        name: 'Cada Momento la Vida me da',
         type: 'himno',
     },
     {
@@ -14,7 +14,94 @@ const anthems =
         number: '3',
         name: 'Nunca Desmayes',
         type: 'himno',
-    }
+    },
+    {
+        number: '4',
+        name: 'No Hay Tristeza En El Reino',
+        type: 'himno',
+    },
+    {
+        number: '5',
+        name: 'Hay un Jardín Memorable',
+        type: 'himno',
+    },
+    {
+        number: '6',
+        name: 'Hay un País',
+        type: 'himno',
+    },
+    {
+        number: '7',
+        name: 'Nunca Dios mío, Cesará mi Labio',
+        type: 'himno',
+    },
+    {
+        number: '8',
+        name: 'El Salvavidas',
+        type: 'himno',
+    },
+    {
+        number: '9',
+        name: 'Obediente a tu Mandato',
+        type: 'himno',
+    },
+    {
+        number: '10',
+        name: 'Cristo, Rey de Gloria',
+        type: 'himno',
+    },
+    {
+        number: '11',
+        name: 'A Jesucristo ven sin Tardar',
+        type: 'himno',
+    },
+    {
+        number: '12',
+        name: 'A solas al huerto',
+        type: 'himno'
+    },
+// Salmos
+    {
+        name: 'Salmo 1',
+        type: 'salmo'
+    },
+    {
+        name: 'Salmo 3',
+        type: 'salmo'
+    },
+    {
+        name: 'Salmo 5',
+        type: 'salmo'
+    },
+    {
+        name: 'Salmo 8',
+        type: 'salmo'
+    },
+    {
+        name: 'Salmo 25',
+        type: 'salmo'
+    },
+// Cantos
+    {
+        name: 'Solamente',
+        type: 'canto'
+    },
+    {
+        name: 'Eres tú',
+        type: 'canto'
+    },
+    {
+        name: 'La Libertad',
+        type: 'canto'
+    },
+    {
+        name: 'Cómo Águila',
+        type: 'canto'
+    },
+    {
+        name: 'Corintios 13',
+        type: 'canto'
+    },
 ]
 
 module.exports = { anthems };
