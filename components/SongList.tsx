c3c035a7298ce1448bdf1bbd49e8df193ebf4b5a
gerardoisai <isai.diaz.perez@alumnos.upm.es>
2023-08-09 20:58:05 -0600

    Add basic repeat counting functionality

--- components/SongList.tsx
+++ components/SongList.tsx
@@ -1,26 +1,45 @@
 'use client'
 
-import { Heading, List, ListItem, Badge, Tag, TagLabel, Highlight } from "@chakra-ui/react";
+import { Heading, List, ListItem, Badge, Tag, TagLabel } from "@chakra-ui/react";
+
+const repeatColors: any = {
+    0: 'teal',
+    1: 'yellow'
+}
 
 export function SongList({ type, songs }: {type: string, songs: Array<any>}) {
     const filteredSongs = songs.filter((song) => song.type === type.toLowerCase())
+
+    const OFFSET = 30 * 24 * 60 * 60 * 1000;
+    const referenceDate = new Date(Date.now() - OFFSET).getTime();
 
     return (
     <>
     <Heading color='white' mb={6}>{type}s</Heading>
         <List color='white' spacing={3}>
         {
-            filteredSongs.map((song: any, i: number) => (
-                <ListItem key={song.id}>
-                    <Tag size='lg' borderRadius='full' variant='outline' colorScheme='teal' mx={1}>
-                        <Badge colorScheme='teal' mr={2}>{song.number ? song.number : i + 1}</Badge>
-                        <TagLabel>{song.name}</TagLabel>
-                    </Tag>
-                    <Tag size='lg' borderRadius='full' variant='solid' colorScheme='teal' mx={1}>
-                        <Badge colorScheme='teal'>1</Badge>
-                    </Tag>
-                </ListItem>
-                ))
+            filteredSongs.map((song: any, i: number) => {
+                const repeats = song.dates.reduce((acc: number, date: any) => {
+                    if(date.getTime() - referenceDate > 0) {
+                        return acc + 1
+                    }
+                    return acc
+                },0);
+
+                const color = repeatColors[repeats] || 'red';
+                
+                return (
+                    <ListItem key={song.id}>
+                        <Tag size='lg' borderRadius='full' variant='outline' colorScheme={color} mx={1}>
+                            <Badge colorScheme={color} mr={2}>{song.number || i + 1}</Badge>
+                            <TagLabel>{song.name}</TagLabel>
+                        </Tag>
+                        <Tag size='lg' borderRadius='full' variant='solid' colorScheme={color} mx={1}>
+                            <Badge colorScheme={color}>{repeats || 0}</Badge>
+                        </Tag>
+                    </ListItem>
+                )
+            })
         }
         </List>
     </>
--- prisma/data.js
+++ prisma/data.js
@@ -4,103 +4,125 @@ const anthems =
         number: '1',
         name: 'Cada Momento la Vida me da',
         type: 'himno',
+        dates: []
     },
     {
         number: '2',
         name: 'Unión Fraternal',
         type: 'himno',
+        dates: []
     },
     {
         number: '3',
         name: 'Nunca Desmayes',
         type: 'himno',
+        dates: []
     },
     {
         number: '4',
         name: 'No Hay Tristeza En El Reino',
         type: 'himno',
+        dates: []
     },
     {
         number: '5',
         name: 'Hay un Jardín Memorable',
         type: 'himno',
+        dates: []
     },
     {
         number: '6',
         name: 'Hay un País',
         type: 'himno',
+        dates: []
     },
     {
         number: '7',
         name: 'Nunca Dios mío, Cesará mi Labio',
         type: 'himno',
+        dates: []
     },
     {
         number: '8',
         name: 'El Salvavidas',
         type: 'himno',
+        dates: []
     },
     {
         number: '9',
         name: 'Obediente a tu Mandato',
         type: 'himno',
+        dates: []
     },
     {
         number: '10',
         name: 'Cristo, Rey de Gloria',
         type: 'himno',
+        dates: []
     },
     {
         number: '11',
         name: 'A Jesucristo ven sin Tardar',
         type: 'himno',
+        dates: []
     },
     {
         number: '12',
         name: 'A solas al huerto',
-        type: 'himno'
+        type: 'himno',
+        dates: []
     },
 // Salmos
     {
         name: 'Salmo 1',
-        type: 'salmo'
+        type: 'salmo',
+        dates: []
     },
     {
         name: 'Salmo 3',
-        type: 'salmo'
+        type: 'salmo',
+        dates: []
     },
     {
         name: 'Salmo 5',
-        type: 'salmo'
+        type: 'salmo',
+        dates: []
     },
     {
         name: 'Salmo 8',
-        type: 'salmo'
+        type: 'salmo',
+        dates: []
     },
     {
         name: 'Salmo 25',
-        type: 'salmo'
+        type: 'salmo',
+        dates: []
     },
 // Cantos
     {
         name: 'Solamente',
-        type: 'canto'
+        type: 'canto',
+        dates: []
     },
     {
         name: 'Eres tú',
-        type: 'canto'
+        type: 'canto',
+        dates: []
     },
     {
         name: 'La Libertad',
-        type: 'canto'
+        type: 'canto',
+        dates: []
     },
     {
         name: 'Cómo Águila',
-        type: 'canto'
+        type: 'canto',
+        dates: []
     },
     {
         name: 'Corintios 13',
-        type: 'canto'
+        type: 'canto',
+        dates: []
     },
 ]
 
