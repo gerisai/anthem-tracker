92987c26c601a5e060b4c893c31e69892b25dc43
gerardoisai <isai.diaz.perez@alumnos.upm.es>
2023-08-11 23:26:20 -0600

    Fix anthem bug not showing number

--- components/ProgramSongList.tsx
+++ components/ProgramSongList.tsx
@@ -6,10 +6,10 @@ export function ProgramSongList({ programSongs, program, deleteProgramSong }:{ p
     return (
         programSongs.map((song: any, i: number) => (
             <Flex direction='row' alignItems='center'>
-            <Highlight key={i} query={`${numberToEmoji(i+1)} ${song.name}`} styles={{ px: '3', py: '1', my:'2', rounded: 'full', bg: 'teal.100' }} >
-                {`${numberToEmoji(i+1)} ${song.name}`}
+            <Highlight key={i} query={`${numberToEmoji(i+1)} ${song.number || ''} ${song.name}`} styles={{ px: '3', py: '1', my:'2', rounded: 'full', bg: 'teal.100' }} >
+                {`${numberToEmoji(i+1)} ${song.number || ''} ${song.name}`}
             </Highlight>
-            <Tag size='lg' colorScheme='red' borderRadius='full' mx={2} onClick={() => deleteProgramSong(program,song)}><BiTrash/></Tag>
+            <Tag as='button' size='lg' colorScheme='red' borderRadius='full' mx={2} onClick={() => deleteProgramSong(program,song)}><BiTrash/></Tag>
             </Flex>
         ))
     )
