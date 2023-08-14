d2b1a065595b30cf34220863f3e187eb6a773795
gerardoisai <isai.diaz.perez@alumnos.upm.es>
2023-08-14 17:26:53 -0600

    Add individual program view

---
+++ app/programs/history/[id]/page.tsx
@@ -0,0 +1,38 @@
+import { CurrentProgram } from '@/components/CurrentProgram';
+import { prisma } from '@/lib/prisma';
+
+interface Props {
+    params: {
+      id: string;
+    };
+};
+
+export default async function Program({ params }: Props) {
+  const selectedFullProgram = await prisma.fullProgram.findUnique({
+    where: {
+      id: params.id
+    },
+    include: {
+      programs: true
+    }
+  });
+
+  let selectedPrograms: any[] = [];
+  
+  for (let p of selectedFullProgram!.programs) {
+    let sp = await prisma.program.findUnique({
+        where: {
+            id: p.id
+        },
+        include: {
+            songs: true
+        },
+    });
+    
+    selectedPrograms = [...selectedPrograms, sp];
+  }
+  
+  return (
+    <CurrentProgram programs={selectedPrograms} historic={true}/>
+  )
+}
