cefd3be81f1efc09e655cee9f1e3aa53fa83a9da
gerardoisai <isai.diaz.perez@alumnos.upm.es>
2023-08-18 12:36:22 -0600

    Make login page client component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,19 @@
-import { getProviders } from "next-auth/react"
+'use client'
+
+import { getProviders, LiteralUnion, ClientSafeProvider } from "next-auth/react";
+import { useState, useEffect } from 'react';
 import SignIn from "@/components/SignIn";
 
-export default async function SignInPage() {
+export default function SignInPage() {
+    const [providers, setProviders] = useState({} as Record<LiteralUnion<string>, ClientSafeProvider> | null);
 
-    const providers = await getProviders();
+    useEffect(() => {
+        (async () => {
+          const res = await getProviders();
+          setProviders(res);
+        })();
+      }, []);
+    
     return (
         <SignIn providers={providers}/>
     )
